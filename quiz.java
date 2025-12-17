274c12d47c7f9ddfed485212667677598e909a6f
himanshu <gytanshu@gmail.com>
2025-12-18 01:28:24 +0530

    day 2 : complete

--- quiz.java
+++ quiz.java
@@ -1,23 +1,46 @@
 import java.util.Scanner;
 
-public class quiz {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int score = 0;
+public class quiz{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int score = 0;
 
-        System.out.println("Welcome to the Quiz!");
+    String[] questions = {  
 
-        System.out.println("Q1: Java is a ?");
-        System.out.println("a) OS");
-        System.out.println("b) Programming Language");
-        System.out.println("c) Browser");
-        char ans1 = sc.next().charAt(0);
+        "Java Is A ?",
+        "Which  one is a loop?"
+    };
 
-        if (ans1 == 'b') {
-            score++;
+    String[][] options =  {
+        {"A) OS","B) Programming Language", "C) Browser"},
+        {"A) If", "B) For","C) Break" }
+    };
+    char[] answers = {'b', 'b'};
+    System.out.println("WELCOME To THe Quiz !");
+
+    for ( int i = 0; i < questions.length; i++) {
+        System.out.println("\nQ" + (i + 1) + ": " + questions[i]);
+
+        for (int j = 0; j   < options[i].length; j++) {
+            System.out.println(options[i][j]);
         }
 
-        System.out.println("Your score is: " + score);
-        sc.close();
+        System.out.println("Your Answer : ");
+        char userAns = sc.next().charAt(0);
+
+        if (userAns == answers[i]) {
+            score++;
+            
+        }
+
     }
+
+    System.out.println("\nQuiz Finished!");
+    System.out.println("Your score: is: " + score + "/" + questions.length);
+
+    sc.close();
+
 }
+
+}
+    
