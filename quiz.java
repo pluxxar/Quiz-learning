b291574ee5e9fb4a799d33d08e9cbd5d65c32cd3
himanshu <gytanshu@gmail.com>
2025-12-18 01:34:12 +0530

    day 3 complete

--- quiz.java
+++ quiz.java
@@ -1,46 +1,49 @@
 import java.util.Scanner;
 
-public class quiz{
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int score = 0;
+public class quiz {
+    public static void main(String[] args) {
 
-    String[] questions = {  
+        Scanner sc = new Scanner(System.in);
+        int score = 0;
 
-        "Java Is A ?",
-        "Which  one is a loop?"
-    };
+        String[] questions = {
+            "Java is a ?",
+            "Which one is a loop?"
+        };
 
-    String[][] options =  {
-        {"A) OS","B) Programming Language", "C) Browser"},
-        {"A) If", "B) For","C) Break" }
-    };
-    char[] answers = {'b', 'b'};
-    System.out.println("WELCOME To THe Quiz !");
+        String[][] options = {
+            {"A) OS", "B) Programming Language", "C) Browser"},
+            {"A) If", "B) For", "C) Break"}
+        };
 
-    for ( int i = 0; i < questions.length; i++) {
-        System.out.println("\nQ" + (i + 1) + ": " + questions[i]);
+        char[] answers = {'b', 'b'};
 
-        for (int j = 0; j   < options[i].length; j++) {
-            System.out.println(options[i][j]);
+        System.out.println("WELCOME TO THE QUIZ!");
+
+        for (int i = 0; i < questions.length; i++) {
+
+            System.out.println("\nQ" + (i + 1) + ": " + questions[i]);
+
+            for (int j = 0; j < options[i].length; j++) {
+                System.out.println(options[i][j]);
+            }
+
+            System.out.print("Your Answer (a/b/c): ");
+            char userAns = Character.toLowerCase(sc.next().charAt(0));
+
+            if (userAns != 'a' && userAns != 'b' && userAns != 'c') {
+                System.out.println("Invalid choice. Skipping question.");
+                continue;
+            }
+
+            if (userAns == answers[i]) {
+                score++;
+            }
         }
 
-        System.out.println("Your Answer : ");
-        char userAns = sc.next().charAt(0);
+        System.out.println("\nQuiz Finished!");
+        System.out.println("Your Score: " + score + "/" + questions.length);
 
-        if (userAns == answers[i]) {
-            score++;
-            
-        }
-
+        sc.close();
     }
-
-    System.out.println("\nQuiz Finished!");
-    System.out.println("Your score: is: " + score + "/" + questions.length);
-
-    sc.close();
-
 }
-
-}
-    
